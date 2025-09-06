8a1d6003af42a734454397ca20c23a205e362954
junhoraymondseo <junseo@cisco.com>
2025-09-06 17:34:25 +0900

    commit again

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// 파일명: HelloWorld.java
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // 콘솔에 메시지 출력
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World22");
+    }
+}
